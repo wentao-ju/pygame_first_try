e830b183371b91b78bd3cf6b2ac3cc0095831875
wentao-ju <wtju68@gmail.com>
2023-03-11 11:49:35 -0500

    modify main.py

--- main.py
+++ main.py
@@ -14,12 +14,32 @@ def obstacle_movement(obstacle_list):
     if obstacle_list:
         for obstacle_rect in obstacle_list:
             obstacle_rect.x -= 5
+            if obstacle_rect.bottom == 300:
+                screen.blit(snail_surface,obstacle_rect)
+            else:
+                screen.blit(fly_surface,obstacle_rect)
 
-            screen.blit(snail_surface,obstacle_rect)
-
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
         return obstacle_list
     else:
         return []
+
+def collisions(player,obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect):
+                return False
+    return True
+
+def player_animation():
+    global player_surface, player_index
+    if player_rect.bottom < 300:
+        player_surface = player_jump
+    else:
+        player_index += 0.1
+        if player_index >= len(player_walk): 
+            player_index = 0
+        player_surface = player_walk[int(player_index)]
 
 pygame.init()  #初始化运行pygame，此函数必须执行
 screen = pygame.display.set_mode((800,400))  #创建游戏窗口
@@ -38,15 +58,28 @@ sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 #.render(text, 是否让字体平滑, color)
 #文本层面
-# score_surface = score_font.render('My Game', False, (64,64,64)).convert()
-# score_rect = score_surface.get_rect(center=(400,50))
 #.convert_alpha()转换图片格式并消除周边白背景
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surface.get_rect(bottomleft=(800,300))  #给snail确定边框
 
+#怪物
+snail_frame1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_frame2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
+snail_frames = [snail_frame1,snail_frame2]
+snail_frame_index = 0
+snail_surface = snail_frames[snail_frame_index]
+fly_frame1 = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
+fly_frame2 = pygame.image.load('graphics/fly/fly2.png').convert_alpha()
+fly_frames = [fly_frame1,fly_frame2]
+fly_frame_index = 0
+fly_surface = fly_frames[fly_frame_index]
 obstacle_rect_list = []
 
-player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+#玩家
+player_walk1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_walk2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha()
+player_walk = [player_walk1,player_walk2]
+player_index = 0
+player_surface = player_walk[player_index]
+player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom=(80,300))  #给人物确定边框
 player_gravity = 0
 
@@ -65,6 +98,12 @@ game_message_rect = game_message.get_rect(center=(400,330))
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer,1500)
 
+snail_animation_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(snail_animation_timer,500)
+
+fly_animation_timer = pygame.USEREVENT + 3
+pygame.time.set_timer(fly_animation_timer,200)
+
 while True:  #保持游戏始终运行
     for event in pygame.event.get():  #获取玩家操作
         if event.type == pygame.QUIT:  #如果用户执行退出操作
@@ -80,43 +119,55 @@ while True:  #保持游戏始终运行
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True  #如果game over，按空格键重新开始
-                snail_rect.left = 800  #初始化snail的位置
+                # snail_rect.left = 800  #初始化snail的位置
                 start_time = int(pygame.time.get_ticks()/1000)  #分数归零
+        
+        if game_active:
+            if event.type == obstacle_timer:
+                if randint(0,2):
+                    obstacle_rect_list.append(snail_surface.get_rect(bottomleft=(randint(900,1100),300)))
+                else:
+                    obstacle_rect_list.append(fly_surface.get_rect(bottomleft=(randint(900,1100),200)))
+            
+            if event.type == snail_animation_timer:
+                if snail_frame_index == 0:
+                    snail_frame_index = 1
+                else:
+                    snail_frame_index = 0
+                snail_surface = snail_frames[snail_frame_index]
 
-        if event.type == obstacle_timer and game_active:
-            obstacle_rect_list.append(snail_surface.get_rect(bottomleft=(randint(900,1100),300)))
-
+            if event.type == fly_animation_timer:
+                if fly_frame_index == 0:
+                    fly_frame_index = 1
+                else:
+                    fly_frame_index = 0
+                fly_surface = fly_frames[fly_frame_index]
+        
     if game_active:
         #层面是可按加载顺序向上叠加的
         screen.blit(sky_surface,(0,0))  #让层面展现到窗口中,左上角为origin
         screen.blit(ground_surface,(0,300))
-        #对指定区域画图案
-        # pygame.draw.rect(screen,'#c0e8ec',score_rect)
-        # pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
         score = display_score()
-
-        #怪物
-        # snail_rect.left -= 4  #让snail向左以每秒3个pixel的距离移动
-        # if snail_rect.right < -100:
-        #     snail_rect.left = 800
-        # screen.blit(snail_surface,snail_rect)
 
         #玩家
         player_gravity += 0.8
         player_rect.y += player_gravity  #实现人物下落
         if player_rect.bottom >= 300:  #让人物停落在地面上
             player_rect.bottom = 300
+        player_animation()
         screen.blit(player_surface,player_rect)
 
         #obstacle movement
         obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
-        #game over state
-        if snail_rect.colliderect(player_rect):
-            game_active = False
+        #collisions
+        game_active = collisions(player_rect,obstacle_rect_list)
     else:  #if game over
         screen.fill((94,129,162))
         screen.blit(player_stand,player_stand_rect)
+        obstacle_rect_list.clear()
+        player_rect.midbottom = (80,300)
+        player_gravity = 0
         score_message = score_font.render(f'Your score: {score}',False,(111,196,169))
         score_message_rect = score_message.get_rect(center=(400,330))
         screen.blit(game_name,game_name_rect)
